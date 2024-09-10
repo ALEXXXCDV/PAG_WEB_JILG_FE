ffb133a63517b26ae88d24d5be1688891e9e9b2e
Alejandro Cadavid <alecad76.ac@gmail.com>
2024-09-10 14:43:54 -0500

    Corrección JILG y Login Exitoso

--- JILG.js
+++ JILG.js
@@ -10,12 +10,12 @@ window.onload = (event) =>{
         const password  = document.getElementById('password').value;
 
         try {
-            const response = await fetch('localhost:3000/api/login', {
+            const response = await fetch('http://localhost:3000/api/login', {
                 method: 'POST',
                 headers:{
                     'Content-Type': 'application/json'
                 },
-                body: JASON.stringyfy({"username": username, "password": password}),
+                body: JSON.stringify({"username": username, "password": password}),
             });
 
             const data = await response.json();
